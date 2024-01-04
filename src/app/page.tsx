3d128cd8e2f4c4cd8fa1f3e9e0232f4c91f12f6b
Bruno Herdina <brunoherdina@outlook.com>
2024-01-04 08:01:04 -0300

    Changes on banner.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,17 @@ const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
-    <div className='relative h-full lg:h-[140vh]'>
-      <main className='relative pb-24 pl-4 lg:space-y-24 lg:pl-16'>
+    <div className='relative h-screen overflow-hidden bg-gradient-to-b from-transparent to-black lg:h-[140vh]'>
+      <main className='relative pb-24 pl-4 lg:pl-16'>
         <div className='flex flex-col space-y-2 py-16 md:space-y-4 lg:h-[65vh] lg:justify-end lg:pb-12'>
-          <div className='absolute left-0 top-0 -z-10 flex h-[95vh] w-screen flex-col bg-black'></div>
+          <div className='absolute left-0 top-0 -z-10 flex h-[95vh] w-screen flex-col bg-black'>
+            <Image
+              src='/the_witcher_banner.jpg'
+              alt='The Witcher'
+              fill={true}
+              className='h-[65vh] object-cover object-top lg:h-[95vh]'
+            />
+          </div>
 
           <h1 className='text-2xl font-bold md:text-4xl lg:text-7xl'>
             The Witcher
@@ -20,13 +27,15 @@ export default function Home() {
           </p>
         </div>
 
-        <button className='w-24 rounded-full bg-white px-4 py-2 font-bold text-black md:w-32 lg:w-40'>
-          Play
-        </button>
+        <div className='flex space-x-3'>
+          <button className='flex cursor-pointer items-center gap-x-2 rounded bg-white px-5 py-1.5 text-sm font-semibold text-black transition hover:opacity-75 md:px-8 md:py-2.5 md:text-xl'>
+            Play
+          </button>
 
-        <button className='w-24 rounded-full bg-gray-700 px-4 py-2 font-bold text-white md:w-32 lg:w-40'>
-          More info
-        </button>
+          <button className='flex cursor-pointer items-center gap-x-2 rounded bg-gray-600 px-5 py-1.5 text-sm font-semibold text-white transition hover:opacity-75 md:px-8 md:py-2.5 md:text-xl'>
+            More info
+          </button>
+        </div>
       </main>
     </div>
   );

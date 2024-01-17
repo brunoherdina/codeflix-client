a02c16e762231c57745f655bd22db171aa116385
Bruno Herdina <brunoherdina@outlook.com>
2024-01-17 08:34:23 -0300

    dynamic route with not found page

---
+++ src/app/settings/user/[id]/not-found.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default async function NotFound() {
+  return (
+    <div>
+      <h1 className='text-2xl font-bold'>Sorry, we could not find this User</h1>
+    </div>
+  );
+}

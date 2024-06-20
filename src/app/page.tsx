c2412db473add9d98bb005eae4c158502b2e9755
Bruno Herdina <brunoherdina@outlook.com>
2024-06-20 08:38:56 -0300

    Get featured movie from api and play video on lg screens

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,17 @@ import { Inter } from 'next/font/google';
 import Header from './components/Header';
 import { MovieRow } from './components/MovieRow';
 import { Banner } from './components/Banner';
+import { getFeaturedMovie } from './service/MovieService';
 
 const inter = Inter({ subsets: ['latin'] });
 
-export default function Home() {
+export default async function Home() {
+  const featuredMovie = await getFeaturedMovie('106');
   return (
     <div className='relative h-screen overflow-hidden bg-gradient-to-b from-transparent to-black lg:h-[140vh]'>
       <Header />
       <main className='relative pb-24 pl-4 lg:pl-16'>
-        <Banner />
+        <Banner movie={featuredMovie} />
         <MovieRow sectionTitle='Trending Now' />
         <MovieRow sectionTitle='Top Rated' />
         <MovieRow sectionTitle='Action Movies' />

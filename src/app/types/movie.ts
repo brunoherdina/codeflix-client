318cfb5995f2438505172e04104efe14a345a0bf
Bruno Herdina <brunoherdina@outlook.com>
2024-06-20 08:15:40 -0300

    Movie and Movies types

---
+++ src/app/types/movie.ts
@@ -0,0 +1,15 @@
+export interface Movie {
+    id:            string;
+    title:         string;
+    description:   string;
+    yearLaunched:  string;
+    link:          string;
+    castMembers:   string[];
+    genres:        string[];
+    thumbFileURL:  string;
+    bannerFileURL: string;
+    videoFileURL:  string;
+    rating:        string;
+}
+
+export type Movies = Movie[];
